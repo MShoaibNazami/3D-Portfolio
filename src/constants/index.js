05e224811c88dfbbb51a9cdf30a62a8e0dba3482
MShoaibNazami <shoaib14499@ucp.edu.pk>
2024-10-16 23:54:04 +0500

    update the data

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,47 +1,25 @@
 import {
-  backend,
-  creator,
-  javascript,
-  typescript,
-  html,
-  css,
-  reactjs,
+  backend, creator, javascript,
+  html, css,
   tailwind,
-  nodejs,
-  mongodb,
-  git,
-  php,
-  mysql,
-  mui,
-  wordpress,
-  elementor,
-  // byteBunch,
-  // bytebunchBlog,
-  // devBrains,
-  // barberShop,
-  // byteKiDuniya,
-  // meharHerbals,
-  // trackomatric,
-  // warraichTraders,
-  // waveslines,
-  // allInOneMedia,
-  // cacheStore,
+  git, php,
+  mysql, mui,
 } from "../assets";
 
 export const socialLinks = [
   {
     name: "Whatsapp",
-    link: "https://web.whatsapp.com/send?phone=923076051825",
+    link: "https://web.whatsapp.com/send?phone=923006673009",
     icon: "fa-brands fa-whatsapp",
   },
   {
     name: "Facebook",
-    link: "https://www.facebook.com/m.shoaib.nazamii",
+    link: "https://www.facebook.com/moizkhan6",
     icon: "fa-brands fa-facebook",
   },
   {
     name: "Instagram",
-    link: "https://www.instagram.com/muhammad_shoaib_nazami/",
+    link: "https://www.instagram.com/m_moiz_k/",
     icon: "fa-brands fa-instagram",
   },
   {
@@ -51,17 +29,17 @@ export const socialLinks = [
   },
   {
     name: "LinkedIn",
-    link: "https://www.linkedin.com/in/m-shoaib-nazami/",
+    link: "https://www.linkedin.com/in/moiz-khan-developer/",
     icon: "fa-brands fa-linkedin",
   },
   {
     name: "Google",
-    link: "mailto:m.shoaib.nazami@gmail.com",
+    link: "mailto:dev.moiz.khan@gmail.com",
     icon: "fa-brands fa-google",
   },
   {
     name: "Phone",
-    link: "tel:+923076051825",
+    link: "tel:+923006673009",
     icon: "fa-solid fa-phone",
   },
 ];
@@ -106,36 +84,12 @@ const technologies = [
     icon: javascript,
   },
   {
-    name: "TypeScript",
-    icon: typescript,
-  },
-  {
-    name: "React JS",
-    icon: reactjs,
-  },
-  {
     name: "Tailwind CSS",
     icon: tailwind,
   },
   {
-    name: "Node JS",
-    icon: nodejs,
-  },
-  {
-    name: "MongoDB",
-    icon: mongodb,
-  },
-  {
     name: "git",
     icon: git,
-  },
-  {
-    name: "Wordpress",
-    icon: wordpress,
-  },
-  {
-    name: "Elementor",
-    icon: elementor,
   },
   {
     name: "PHP",
@@ -179,324 +133,28 @@ const testimonials = [
 ];
 
 const projects = [
-  // {
-  //   name: "ByteBunch",
-  //   description:
-  //     "ByteBunch is an IT Services Provider Company that provides many services like Web Development, Graphic development, and many more.",
-  //   tags: [
-  //     {
-  //       name: "html5",
-  //       color: "blue-text-gradient",
-  //     },
-  //     {
-  //       name: "css3",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "bootstrap5",
-  //       color: "pink-text-gradient",
-  //     },
-  //     {
-  //       name: "wordpress",
-  //       color: "blue-text-gradient",
-  //     },
-  //     {
-  //       name: "php",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "mysql",
-  //       color: "pink-text-gradient",
-  //     },
-  //   ],
-  //   image: byteBunch,
-  //   source_code_link: "https://github.com/",
-  //   source_link: "https://amiraliliaqat.github.io/ByteBunch-Html/",
-  // },
-  // {
-  //   name: "ByteBunch Blog",
-  //   description:
-  //     "ByteBunch Blog is the blogging site for ByteBunch IT services provider company to aware the peoples with latest information and updates.",
-  //   tags: [
-  //     {
-  //       name: "html5",
-  //       color: "blue-text-gradient",
-  //     },
-  //     {
-  //       name: "css3",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "bootstrap5",
-  //       color: "pink-text-gradient",
-  //     },
-  //     {
-  //       name: "wordpress",
-  //       color: "blue-text-gradient",
-  //     },
-  //     {
-  //       name: "php",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "mysql",
-  //       color: "pink-text-gradient",
-  //     },
-  //   ],
-  //   image: bytebunchBlog,
-  //   source_code_link: "https://github.com/",
-  //   source_link: "https://amiraliliaqat.github.io/Test-ByteBunch-Html/",
-  // },
-  // {
-  //   name: "Byte Ki Duniya",
-  //   description:
-  //     "Byte Ki Duniya is an IT services provider company that provides all the ultimate solutions for your business.",
-  //   tags: [
-  //     {
-  //       name: "html5",
-  //       color: "blue-text-gradient",
-  //     },
-  //     {
-  //       name: "css3",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "bootstrap5",
-  //       color: "pink-text-gradient",
-  //     },
-  //     {
-  //       name: "wordpress",
-  //       color: "blue-text-gradient",
-  //     },
-  //     {
-  //       name: "php",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "mysql",
-  //       color: "pink-text-gradient",
-  //     },
-  //   ],
-  //   image: byteKiDuniya,
-  //   source_code_link: "https://github.com/",
-  //   source_link: "http://byte.likesyou.org/",
-  // },
-  // {
-  //   name: "DevBrains",
-  //   description:
-  //     "DevBrains is an IT Services Provider Company that provides many services like Web Development, Graphic development, and many more.",
-  //   tags: [
-  //     {
-  //       name: "wordpress",
-  //       color: "blue-text-gradient",
-  //     },
-  //     {
-  //       name: "elementor",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "react",
-  //       color: "pink-text-gradient",
-  //     },
-  //   ],
-  //   image: devBrains,
-  //   source_code_link: "https://github.com/",
-  //   source_link: "http://devbrains.likesyou.org/",
-  // },
-  // {
-  //   name: "Warraich Traders",
-  //   description:
-  //     "Warriach Traders is the housing society management system that is used for buying houses and managing houses materials.",
-  //   tags: [
-  //     {
-  //       name: "html5",
-  //       color: "blue-text-gradient",
-  //     },
-  //     {
-  //       name: "css3",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "bootstrap5",
-  //       color: "pink-text-gradient",
-  //     },
-  //     {
-  //       name: "wordpress",
-  //       color: "blue-text-gradient",
-  //     },
-  //     {
-  //       name: "php",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "mysql",
-  //       color: "pink-text-gradient",
-  //     },
-  //     {
-  //       name: "react",
-  //       color: "green-text-gradient",
-  //     },
-  //   ],
-  //   image: warraichTraders,
-  //   source_code_link: "https://github.com/",
-  //   source_link: "https://warraich-traders-react.vercel.app/",
-  // },
-  // {
-  //   name: "Mehar Herbals",
-  //   description:
-  //     "Mehar Herbals is an ecommerce site providing various variaty of herbals products to fullfill your herbal needs.",
-  //   tags: [
-  //     {
-  //       name: "html5",
-  //       color: "blue-text-gradient",
-  //     },
-  //     {
-  //       name: "css3",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "bootstrap5",
-  //       color: "pink-text-gradient",
-  //     },
-  //     {
-  //       name: "wordpress",
-  //       color: "blue-text-gradient",
-  //     },
-  //     {
-  //       name: "php",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "woocommerce",
-  //       color: "pink-text-gradient",
-  //     },
-  //   ],
-  //   image: meharHerbals,
-  //   source_code_link: "https://github.com/",
-  //   source_link: "http://meharherbals.likesyou.org/",
-  // },
-  // {
-  //   name: "Trackomatric",
-  //   description:
-  //     "Trakomatic is an artificial intelligence system for detacking security activites at large scale and shopping malls.",
-  //   tags: [
-  //     {
-  //       name: "html5",
-  //       color: "blue-text-gradient",
-  //     },
-  //     {
-  //       name: "css3",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "bootstrap5",
-  //       color: "pink-text-gradient",
-  //     },
-  //   ],
-  //   image: trackomatric,
-  //   source_code_link: "https://github.com/",
-  //   source_link: "https://amiraliliaqat.github.io/Trackomatric/",
-  // },
-  // {
-  //   name: "Barber Shop",
-  //   description:
-  //     "Complate and responseive barber shop template using mern stack with good and attractive user experience and user interface.",
-  //   tags: [
-  //     {
-  //       name: "html5",
-  //       color: "blue-text-gradient",
-  //     },
-  //     {
-  //       name: "css3",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "bootstrap5",
-  //       color: "pink-text-gradient",
-  //     },
-  //     {
-  //       name: "react",
-  //       color: "blue-text-gradient",
-  //     },
-  //   ],
-  //   image: barberShop,
-  //   source_code_link: "https://github.com/",
-  //   source_link: "https://barber-shop-tau-three.vercel.app/",
-  // },
-  // {
-  //   name: "Waveslines",
-  //   description:
-  //     "Waveslines is an ecommerce services provider company which can offers products of all seasons and vatities.",
-  //   tags: [
-  //     {
-  //       name: "wordpress",
-  //       color: "blue-text-gradient",
-  //     },
-  //     {
-  //       name: "elementor",
-  //       color: "green-text-gradient",
-  //     },
-  //   ],
-  //   image: waveslines,
-  //   source_code_link: "https://github.com/",
-  //   source_link: "https://weavelinens.com/",
-  // },
-  // {
-  //   name: "All in one media",
-  //   description:
-  //     "All in one media is the complete and fully featured media solutions application with various modules and features.",
-  //   tags: [
-  //     {
-  //       name: "react",
-  //       color: "blue-text-gradient",
-  //     },
-  //     {
-  //       name: "next.js",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "mui",
-  //       color: "pink-text-gradient",
-  //     },
-  //     {
-  //       name: "node.js",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "express.js",
-  //       color: "pink-text-gradient",
-  //     },
-  //     {
-  //       name: "mongodb",
-  //       color: "blue-text-gradient",
-  //     },
-  //   ],
-  //   image: allInOneMedia,
-  //   source_code_link: "https://github.com/",
-  //   source_link: "https://all-in-one-media.vercel.app",
-  // },
-  // {
-  //   name: "Cache Store",
-  //   description:
-  //     "Cache store is full-featured online mobile accessories selling ecommerce website. Cache store have multiple categories like airbuds, hand-frees, mobile charger etc.",
-  //   tags: [
-  //     {
-  //       name: "wordpress",
-  //       color: "blue-text-gradient",
-  //     },
-  //     {
-  //       name: "elementor",
-  //       color: "green-text-gradient",
-  //     },
-  //     {
-  //       name: "woocommerce",
-  //       color: "pink-text-gradient",
-  //     },
-  //   ],
-  //   image: cacheStore,
-  //   source_code_link: "https://github.com/",
-  //   source_link: "https://cachestore.likesyou.org",
-  // },
+  {
+    name: "Animals Transform",
+    description:
+      "Animal Shape Transforming Race offers an exhilarating and captivating race where you must morph your animal to match the terrain, boosting your speed and leaving competitors in the dust. Step into a world of endless fun where every transformation brings laughter and excitement. 🌟",
+    tags: [
+      {
+        name: "Unity",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "C#",
+        color: "green-text-gradient",
+      },
+      {
+        name: "ShaderGraph",
+        color: "pink-text-gradient",
+      },
+    ],
+    image:"" ,
+    source_code_link: "",
+    source_link: "https://play.google.com/store/apps/details?id=com.my.animal.transform.race.game/",
+  },
 ];
 
 export { services, technologies, testimonials, projects };
